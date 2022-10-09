e85b93c1402e8f0a4f74ba0685af411266943199
dymafr <32801166+dymafr@users.noreply.github.com>
2022-10-09 12:16:27 +0200

    1 file modified

--- src/apis/recipes.js
+++ src/apis/recipes.js
@@ -2,12 +2,6 @@ export async function getRecipes() {
   const response = await fetch('https://restapi.fr/api/recipes');
   if (response.ok) {
     return response.json();
-    // throw new Error("Oops");
-    // return new Promise((res) => {
-    //   setTimeout(() => {
-    //     res(body);
-    //   }, 3000);
-    // });
   } else {
     throw new Error('something went wrong');
   }
